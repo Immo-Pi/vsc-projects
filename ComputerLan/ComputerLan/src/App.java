61c5bdeebb2ff8ba33aea2000ee7cd01ff3e744c
Immo-Pi <kowarzirmio@gmail.com>
2024-09-12 09:46:29 +0200

    Update App.java

--- ComputerLan/ComputerLan/src/App.java
+++ ComputerLan/ComputerLan/src/App.java
@@ -7,14 +7,14 @@ public class App {
         new Teilnehmer("Torben Spahn", 187, 2425, s1).addSeminar(s2);
         new Teilnehmer("Tom R", 188, 2426, s1);
         new Teilnehmer("Paul Fehl", 189, 2427, s1);
-        new Teilnehmer("Toni Tonius", 193, 2429, s1).addSeminar(s2);
 
         new Teilnehmer("Finja Friedrich", 190, 2425, s2).addSeminar(s1);
         new Teilnehmer("Vincent Abels", 191, 2426, s2);
         new Teilnehmer("Tom B", 192, 2427, s2);
         
+        System.out.println("Seminar 1:");
         s1.getTeilnehmer();
-        System.out.println("\n");
+        System.out.println("\nSeminar 2");
         s2.getTeilnehmer();
 
     }
